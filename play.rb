8358c434dea470afcb6678b869cff8eafc65f18d
Leonardo Prado <leonardoprado@gmail.com>
2017-04-15 17:47:01 -0300

    Refactor the play example script

--- play.rb
+++ play.rb
@@ -2,35 +2,39 @@
 
 require_relative 'minesweeper'
 
+def action_prompt
+  print 'Select your action: 1: click / 2: flag (default: 1): '
+  action = gets.chomp
+  case action
+  when '2', 'flag'
+    :flag
+  else
+    :click
+  end
+end
+
+def game_round(game, printer)
+  printer.print_board(game.board_state)
+
+  action = action_prompt
+
+  print 'select a line: '
+  line = Integer(gets)
+
+  print 'select a column: '
+  column = Integer(gets)
+
+  game.send(action, column, line)
+end
+
 def start_game(*args)
   game = Minesweeper::Game.new(*args)
   printer = Minesweeper::SimplePrinter.new
 
-  while game.still_playing?
-    printer.print_board(game.board_state)
-
-    print 'Select your action: 1: click / 2: flag (default: 1): '
-    action = gets.chomp
-    case action
-    when '2', 'flag'
-      action_method = :flag
-    else
-      action_method = :click
-    end
-
-    print 'select a line: '
-    line = Integer(gets)
-
-    print 'select a column: '
-    column = Integer(gets)
-
-    game.send(action_method, column, line)
-  end
+  game_round(game, printer) while game.still_playing?
 rescue Interrupt
   puts "\n\nThanks for playing!"
 end
 
 # start_game(type: :custom, columns: 20, lines: 20, mines: 100)
 start_game(type: :beginner)
-
-# printer.print_board(game.board_state, x_ray: true)
